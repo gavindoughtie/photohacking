beb576d55bfd9abd1d67105417458ceb9b338bfc
Gavin Doughtie <gavin@doughtie.com>
2022-12-19 19:45:25 +0000

    Fetching from unsplash

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import "./styles.css";
 import Photo from "./Photo";
-import { IMAGES } from "./images";
+import { getUnsplashImages } from "./images";
+import { useEffect, useState } from "react";
+
+async function fetchImages(setImages: any) {
+  const data = await getUnsplashImages("landscape");
+  setImages(data);
+}
+
+type ImageData = {
+  src: string;
+};
 
 export default function App() {
-  const photos = IMAGES.map((imgData) => (
-    <Photo src={imgData.src} size={300} alt="Demo" />
-  ));
-  const row = <div className="photoRow">{photos}</div>;
-  return <div className="App">{row}</div>;
+  const [images, setImages] = useState<ImageData[]>();
+  useEffect(() => {
+    if (!images?.length) {
+      fetchImages(setImages);
+    }
+  });
+  let photos;
+  if (images) {
+    photos = images.map((imgData) => (
+      <Photo src={imgData.src} size={300} alt="Demo" />
+    ));
+  } else {
+    photos = <div>LOADING</div>;
+  }
+  return (
+    <div className="App">
+      <div className="gallery">{photos}</div>
+    </div>
+  );
 }
--- src/images.ts
+++ src/images.ts
@@ -1,12 +1,33 @@
-export {}
-
-const IMAGES = [
+export const IMAGES = [
   {
     src: "https://ecologiclandscape.com/wp-content/uploads/2020/12/living-los-angeles-2-848x480.jpg",
   },
   {
     src: "https://www.design-scapes-inc.com/wp-content/uploads/2021/08/los-angeles-landscape-designers.jpg",
   },
+  {
+    src: "https://images.unsplash.com/photo-1484591974057-265bb767ef71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80",
+  },
+  {
+    src: "https://www.nyip.edu/images/cms/photo-articles/the-best-place-to-focus-in-a-landscape.jpg",
+  },
+  {
+    src: "https://media.architecturaldigest.com/photos/60d20f8d9f3cb91f0b9c0e95/16:9/w_1280,c_limit/GL.jpg",
+  },
+  {
+    src: "https://wallpaperaccess.com/full/170249.jpg",
+  },
 ];
 
-export IMAGES;
+export default IMAGES;
+
+export async function getUnsplashImages(subject: string) {
+  const url = `https://api.unsplash.com/photos/?client_id=nWCpf8N35_0nkL6feuo-cCAMq-Ioxw55wohncO-ocZQ&query=${subject}&page=1&per-page=50`;
+  const results = await fetch(url);
+  const data = await results.json();
+  return data.map((item: any) => {
+    return {
+      src: item.urls.small,
+    };
+  });
+}
